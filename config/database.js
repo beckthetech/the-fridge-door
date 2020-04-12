b9a961debc7ab527f35622e23a13d1c00c8c7d26
Daniel Beck <dlc.beck@gmail.com>
2020-04-11 23:06:39 -0700

    update

--- config/database.js
+++ config/database.js
@@ -1,17 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-    'mongodb://localhost:27017/groceries',
-    {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    }
-);
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
-db.on('connected', function () {
-    console.log(`Connectected to MongoDB at ${db.host}:${db.port}`);
+db.once('connected', () => {
+  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
 });
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const logger = require('morgan');
 
 const app = express();
@@ -11,7 +11,7 @@ require('./config/database');
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use(favicon(path.join(__dirname, 'build')));
+// app.use(favicon(path.join(__dirname, 'build')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 const groceriesRouter = require('./routes/api/groceries');
