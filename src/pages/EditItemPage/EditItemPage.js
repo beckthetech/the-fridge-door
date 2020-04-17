3d539749c146964c6adf60a9386aaacab19ebd24
Daniel Beck <dlc.beck@gmail.com>
2020-04-17 12:36:22 -0700

    update protected edit route for user experience

--- src/pages/EditItemPage/EditItemPage.js
+++ src/pages/EditItemPage/EditItemPage.js
@@ -9,8 +9,7 @@ import './EditItemPage.css'
 class EditItemPage extends Component {
     state = {
         invalidForm: true,
-        // user: this.props.location.user,
-        formData: this.props.location.state.item
+        formData: this.props.location.state ? this.props.location.state.item : null
     };
 
     formRef = React.createRef();
@@ -29,54 +28,56 @@ class EditItemPage extends Component {
     };
 
     handleChangeCategories = categories => {
-        this.setState({ formData: { ...this.state.formData, categories } });
+        this.setState({
+            formData: { ...this.state.formData, categories },
+            invalidForm: !this.formRef.current.checkValidity()
+        });
+    }
+
+    renderEditItemForm() {
+        return (
+            <>
+                <h1>Edit Item</h1>
+                <form ref={this.formRef} autoComplete="off" onSubmit={this.handleSubmit}>
+                    <label>Name</label>
+                    <input
+                        name="name"
+                        value={this.state.formData.name}
+                        onChange={this.handleChange}
+                        required
+                    />
+                    <label>Categories</label>
+                    <div className='categories-selector'>
+                        <Select
+                            value={this.state.formData.categories}
+                            isMulti
+                            name="categories"
+                            onChange={this.handleChangeCategories}
+                            options={itemCategories}
+                            required
+                        />
+                    </div>
+                    <label>Description</label>
+                    <input
+                        name="description"
+                        value={this.state.formData.description}
+                        onChange={this.handleChange}
+                        required
+                    />
+                    <button
+                        type="submit"
+                        disabled={this.state.invalidForm}
+                    >
+                        SAVE ITEM
+                    </button>&nbsp;&nbsp;
+                    <Link to='/'>CANCEL</Link>
+                </form>
+            </>
+        )
     }
 
     render() {
-        console.log(this.state.user)
-        // if (this.state.user._id === this.state.formData.user) {
-            return (
-                <>
-                    <h1>Edit Item</h1>
-                    <form ref={this.formRef} autoComplete="off" onSubmit={this.handleSubmit}>
-                        <label>Name</label>
-                        <input
-                            name="name"
-                            value={this.state.formData.name}
-                            onChange={this.handleChange}
-                            required
-                        />
-                        <label>Categories</label>
-                        <div className='categories-selector'>
-                            <Select
-                                value={this.state.formData.categories}
-                                isMulti
-                                name="categories"
-                                onChange={this.handleChangeCategories}
-                                options={itemCategories}
-                                required
-                            />
-                        </div>
-                        <label>Description</label>
-                        <input
-                            name="description"
-                            value={this.state.formData.description}
-                            onChange={this.handleChange}
-                            required
-                        />
-                        <button
-                            type="submit"
-                            disabled={this.state.invalidForm}
-                        >
-                            SAVE ITEM
-                        </button>&nbsp;&nbsp;
-                        <Link to='/'>CANCEL</Link>
-                    </form>
-                </>
-            );
-        /*} else {
-            return < Redirect to='/login' />
-        }*/
+        return this.state.formData ? this.renderEditItemForm() : <Redirect to='/login' />;
     }
 }
 
